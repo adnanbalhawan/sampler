ca8df99ba52f6d6540674db1951d624c137bce94
Adnan Balhawan <adnanbalhawan@gmail.com>
2018-03-06 16:15:35 -0500

    Added sampler code

---
+++ src/index.js
@@ -0,0 +1,45 @@
+// let the fun begin!
+import React from 'react'
+import { render } from 'react-dom'
+import { zip } from 'lodash'
+import WebAudioScheduler from 'web-audio-scheduler'
+import Sampler from './Sampler'
+
+async function app() {
+  const audioCtx = new AudioContext()
+  const samples = ['kick', 'hat', 'snare', 'cowbell']
+
+  const responses = await Promise.all(
+    samples.map(sample => fetch(`/drums/${sample}.wav`)),
+  )
+
+  const bufferMap = await zip(samples, responses).reduce(
+    async (acc, [sample, response]) => {
+      const arrayBuffer = await response.arrayBuffer()
+      const audioBuffer = await audioCtx.decodeAudioData(arrayBuffer)
+      return { ...(await acc), [sample]: audioBuffer }
+    },
+    {},
+  )
+
+  const scheduler = new WebAudioScheduler({ context: audioCtx })
+
+  function trigger(event) {
+    const source = audioCtx.createBufferSource()
+    source.buffer = event.args.buffer
+    source.connect(audioCtx.destination)
+    source.start(event.playbackTime)
+  }
+
+  return {
+    scheduler,
+    audioCtx,
+    samples,
+    bufferMap,
+    trigger,
+  }
+}
+
+app().then(audioProps =>
+  render(<Sampler {...audioProps} />, document.getElementById('root')),
+)
